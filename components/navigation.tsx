747b1418270732a5dd1402f0c5be017708e51b71
cwon07 <jumiryoo@gmail.com>
2024-07-28 20:56:01 -0700

    deleted Settings from the navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -28,10 +28,6 @@ const routes = [
         href: "/categories",
         label: "Categories",
     },
-    {
-        href: "/settings",
-        label: "Settings",
-    },    
 ];
 
 export const Navigation = () => {
